84d04cd2c1887571fb58ab7d4419a75f83a91cea
Jackson Chen <tiger2chen@gmail.com>
2014-12-01 14:01:24 -0700

    Adds PDF

---
+++ classes/__SHELL1.java
@@ -0,0 +1,8 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+ColorChooser.main(__bluej_param0);
+
+}}
